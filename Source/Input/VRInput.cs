8dd509558b5cac85ed0d274cc5e41e67b4fa7799
LeCloutPanda <53411604+LeCloutPanda@users.noreply.github.com>
2025-01-05 11:19:06 +1100

    Removed Tabs and Replaced with spaces

--- Source/Input/VRInput.cs
+++ Source/Input/VRInput.cs
@@ -6,41 +6,41 @@ namespace Aquamarine.Source.Input;
 
 public partial class VRInput : Node3D, IInputProvider
 {
-	[Export] private XROrigin3D _origin;
-	[Export] private XRController3D _leftHand;
-	[Export] private XRController3D _rightHand;
-	[Export] private XRCamera3D _head;
-	private Vector3 _playspaceDelta;
+    [Export] private XROrigin3D _origin;
+    [Export] private XRController3D _leftHand;
+    [Export] private XRController3D _rightHand;
+    [Export] private XRCamera3D _head;
+    private Vector3 _playspaceDelta;
     private float _rotation;
 
-	public static readonly PackedScene PackedScene = ResourceLoader.Load<PackedScene>("res://Scenes/VRInput.tscn");
+    public static readonly PackedScene PackedScene = ResourceLoader.Load<PackedScene>("res://Scenes/VRInput.tscn");
 
-	public override void _Ready()
-	{
-		try
-		{
-			base._Ready();
+    public override void _Ready()
+    {
+        try
+        {
+            base._Ready();
 
-			IInputProvider.Instance = this;
+            IInputProvider.Instance = this;
 
-			ProcessPriority = -9;
-			_origin.ProcessPriority = -10;
-			_head.ProcessPriority = -10;
-			_leftHand.ProcessPriority = -10;
-			_rightHand.ProcessPriority = -10;
+            ProcessPriority = -9;
+            _origin.ProcessPriority = -10;
+            _head.ProcessPriority = -10;
+            _leftHand.ProcessPriority = -10;
+            _rightHand.ProcessPriority = -10;
 
-			Logger.Log("XRInput initialized successfully.");
-		}
-		catch (Exception ex)
-		{
-			Logger.Error($"Error during XRInput initialization: {ex.Message}");
-		}
-	}
+            Logger.Log("XRInput initialized successfully.");
+        }
+        catch (Exception ex)
+        {
+            Logger.Error($"Error during XRInput initialization: {ex.Message}");
+        }
+    }
 
 
-	public override void _Process(double delta)
-	{
-		base._Process(delta);
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
 
         var deltaf = (float)delta;
 
@@ -54,19 +54,19 @@ public partial class VRInput : Node3D, IInputProvider
         
         var newOffset = -((globalInverse * _head.GlobalTransform).Origin with { Y = 0 });
         
-		//var headPos = _head.Position with { Y = 0 };
-		//var currentOriginOffset = _origin.Position with { Y = 0 };
+        //var headPos = _head.Position with { Y = 0 };
+        //var currentOriginOffset = _origin.Position with { Y = 0 };
         
-		_playspaceDelta = playDelta;
-		_origin.Position += newOffset;
-	}
-	public bool IsVR => true;
-	public Vector3 GetPlayspaceMovementDelta => _playspaceDelta;
-	public Vector2 GetMovementInputAxis => _leftHand.GetVector2("primary") * new Vector2(1, -1);
+        _playspaceDelta = playDelta;
+        _origin.Position += newOffset;
+    }
+    public bool IsVR => true;
+    public Vector3 GetPlayspaceMovementDelta => _playspaceDelta;
+    public Vector2 GetMovementInputAxis => _leftHand.GetVector2("primary") * new Vector2(1, -1);
     public bool GetJumpInput => false;
     public bool GetSprintInput => false;
     public float GetHeight => 1.8f; //TODO
-	public Vector3 GetLimbPosition(IInputProvider.InputLimb limb) => _origin.Transform.TranslatedLocal(limb switch
+    public Vector3 GetLimbPosition(IInputProvider.InputLimb limb) => _origin.Transform.TranslatedLocal(limb switch
     {
         IInputProvider.InputLimb.Head => _head.Position,
         IInputProvider.InputLimb.LeftHand => _leftHand.Position,
@@ -76,7 +76,7 @@ public partial class VRInput : Node3D, IInputProvider
         //IInputProvider.InputLimb.RightFoot => expr,
         _ => -_origin.Position,
     }).Origin;
-	public Quaternion GetLimbRotation(IInputProvider.InputLimb limb) => _origin.Quaternion * limb switch
+    public Quaternion GetLimbRotation(IInputProvider.InputLimb limb) => _origin.Quaternion * limb switch
     {
         IInputProvider.InputLimb.Head => _head.Quaternion,
         IInputProvider.InputLimb.LeftHand => _leftHand.Quaternion,
@@ -86,5 +86,5 @@ public partial class VRInput : Node3D, IInputProvider
         //IInputProvider.InputLimb.RightFoot => expr,
         _ => _origin.Quaternion.Inverse(),
     };
-	public void MoveTransform(Transform3D transform) => GlobalTransform = transform;
+    public void MoveTransform(Transform3D transform) => GlobalTransform = transform;
 }
--- Source/Management/MultiplayerScene.cs
+++ Source/Management/MultiplayerScene.cs
@@ -9,30 +9,30 @@ using Godot.Collections;
 
 namespace Aquamarine.Source.Management
 {
-	public partial class MultiplayerScene : Node
-	{
-		public static MultiplayerScene Instance;
+    public partial class MultiplayerScene : Node
+    {
+        public static MultiplayerScene Instance;
 
-		[Export] public MultiplayerSpawner Spawner;
-		[Export] public MultiplayerSpawner PlayerSpawner;
-		[Export] public Node3D PlayerRoot;
-		[Export] public Array<int> PlayerList = [];
+        [Export] public MultiplayerSpawner Spawner;
+        [Export] public MultiplayerSpawner PlayerSpawner;
+        [Export] public Node3D PlayerRoot;
+        [Export] public Array<int> PlayerList = [];
 
-		public System.Collections.Generic.Dictionary<string, Prefab> Prefabs = new();
+        public System.Collections.Generic.Dictionary<string, Prefab> Prefabs = new();
         
-		public override void _Ready()
-		{
-			base._Ready();
-			Instance = this;
-			Logger.Log("MultiplayerScene initialized.");
-		}
-		public void SendUpdatedPlayerList()
-		{
-			if (IsMultiplayerAuthority()) Rpc(MethodName.UpdatePlayerList, PlayerList.ToArray());
-		}
+        public override void _Ready()
+        {
+            base._Ready();
+            Instance = this;
+            Logger.Log("MultiplayerScene initialized.");
+        }
+        public void SendUpdatedPlayerList()
+        {
+            if (IsMultiplayerAuthority()) Rpc(MethodName.UpdatePlayerList, PlayerList.ToArray());
+        }
 
-		[Rpc(CallLocal = false, TransferChannel = SerializationHelpers.WorldUpdateChannel, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
-		private void UpdatePlayerList(int[] playerList) => PlayerList = new Array<int>(playerList);
+        [Rpc(CallLocal = false, TransferChannel = SerializationHelpers.WorldUpdateChannel, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
+        private void UpdatePlayerList(int[] playerList) => PlayerList = new Array<int>(playerList);
         
         public void SendAllPrefabs(int? user = null)
         {
@@ -56,71 +56,71 @@ namespace Aquamarine.Source.Management
             
         }
         
-		
-		//[Rpc(CallLocal = true, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable, TransferChannel = SerializationHelpers.WorldUpdateChannel)]
-		private void InternalSpawnPlayer(int authority, Vector3 position)
-		{
-			try
-			{
-				var charController = PlayerCharacterController.PackedScene.Instantiate<PlayerCharacterController>();
-				charController.SetPlayerAuthority(authority);
-				charController.Name = authority.ToString();
-				PlayerRoot.AddChild(charController);
-				PlayerRoot.GlobalPosition = position;
-				Logger.Log($"Player with authority {authority} spawned at position {position}.");
-			}
-			catch (Exception ex)
-			{
-				Logger.Error($"Error spawning player with authority {authority}: {ex.Message}");
-			}
-		}
+        
+        //[Rpc(CallLocal = true, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable, TransferChannel = SerializationHelpers.WorldUpdateChannel)]
+        private void InternalSpawnPlayer(int authority, Vector3 position)
+        {
+            try
+            {
+                var charController = PlayerCharacterController.PackedScene.Instantiate<PlayerCharacterController>();
+                charController.SetPlayerAuthority(authority);
+                charController.Name = authority.ToString();
+                PlayerRoot.AddChild(charController);
+                PlayerRoot.GlobalPosition = position;
+                Logger.Log($"Player with authority {authority} spawned at position {position}.");
+            }
+            catch (Exception ex)
+            {
+                Logger.Error($"Error spawning player with authority {authority}: {ex.Message}");
+            }
+        }
 
-		//[Rpc(CallLocal = true, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable, TransferChannel = SerializationHelpers.WorldUpdateChannel)]
-		private void InternalRemovePlayer(int authority)
-		{
-			try
-			{
-				var charController = PlayerRoot.GetChildren().FirstOrDefault(i => i is PlayerCharacterController cont && cont.Authority == authority);
-				if (charController is null)
-				{
-					Logger.Warn($"Attempted to remove player with authority {authority}, but they were not found.");
-					return;
-				}
-				PlayerRoot.RemoveChild(charController);
-				charController.QueueFree();
-				Logger.Log($"Player with authority {authority} removed.");
-			}
-			catch (Exception ex)
-			{
-				Logger.Error($"Error removing player with authority {authority}: {ex.Message}");
-			}
-		}
-		public void SpawnPlayer(int authority, Vector3? position = null)
-		{
-			if (IsMultiplayerAuthority())
-			{
-				InternalSpawnPlayer(authority, position ?? Vector3.Zero);
-				//Rpc(MethodName.InternalSpawnPlayer, authority, position ?? Vector3.Zero);
-				Logger.Log($"SpawnPlayer called for authority {authority}.");
-			}
-			else
-			{
-				Logger.Warn($"SpawnPlayer call ignored due to lack of multiplayer authority for authority {authority}.");
-			}
-		}
+        //[Rpc(CallLocal = true, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable, TransferChannel = SerializationHelpers.WorldUpdateChannel)]
+        private void InternalRemovePlayer(int authority)
+        {
+            try
+            {
+                var charController = PlayerRoot.GetChildren().FirstOrDefault(i => i is PlayerCharacterController cont && cont.Authority == authority);
+                if (charController is null)
+                {
+                    Logger.Warn($"Attempted to remove player with authority {authority}, but they were not found.");
+                    return;
+                }
+                PlayerRoot.RemoveChild(charController);
+                charController.QueueFree();
+                Logger.Log($"Player with authority {authority} removed.");
+            }
+            catch (Exception ex)
+            {
+                Logger.Error($"Error removing player with authority {authority}: {ex.Message}");
+            }
+        }
+        public void SpawnPlayer(int authority, Vector3? position = null)
+        {
+            if (IsMultiplayerAuthority())
+            {
+                InternalSpawnPlayer(authority, position ?? Vector3.Zero);
+                //Rpc(MethodName.InternalSpawnPlayer, authority, position ?? Vector3.Zero);
+                Logger.Log($"SpawnPlayer called for authority {authority}.");
+            }
+            else
+            {
+                Logger.Warn($"SpawnPlayer call ignored due to lack of multiplayer authority for authority {authority}.");
+            }
+        }
 
-		public void RemovePlayer(int authority)
-		{
-			if (IsMultiplayerAuthority())
-			{
-				InternalRemovePlayer(authority);
-				//Rpc(MethodName.InternalRemovePlayer, authority);
-				Logger.Log($"RemovePlayer called for authority {authority}.");
-			}
-			else
-			{
-				Logger.Warn($"RemovePlayer call ignored due to lack of multiplayer authority for authority {authority}.");
-			}
-		}
-	}
+        public void RemovePlayer(int authority)
+        {
+            if (IsMultiplayerAuthority())
+            {
+                InternalRemovePlayer(authority);
+                //Rpc(MethodName.InternalRemovePlayer, authority);
+                Logger.Log($"RemovePlayer called for authority {authority}.");
+            }
+            else
+            {
+                Logger.Warn($"RemovePlayer call ignored due to lack of multiplayer authority for authority {authority}.");
+            }
+        }
+    }
 }

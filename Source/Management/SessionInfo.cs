c882f4fa15362cb4a0370317ccfb62c9a30f8f8f
xLinka <Oliver.dunnington@outlook.com>
2025-01-19 07:28:30 +0000

    New Endpoints Lumora domain

--- Source/Management/SessionInfo.cs
+++ Source/Management/SessionInfo.cs
@@ -5,10 +5,10 @@ namespace Aquamarine.Source.Management;
 
 public class SessionInfo
 {
-    public static IPEndPoint SessionServer = NetUtils.MakeEndPoint("relay.xlinka.com", 8000);
-    public static IPEndPoint RelayServer = NetUtils.MakeEndPoint("relay.xlinka.com", 8090);
-    public static string SessionList = "https://api.xlinka.com/sessions/";
-    
+    public static IPEndPoint SessionServer = NetUtils.MakeEndPoint("backend.lumoravr.com", 8000);
+    public static IPEndPoint RelayServer = NetUtils.MakeEndPoint("backend.lumoravr.com", 8090);
+    public static string SessionList = "https://api.lumoravr.com/sessions";
+
     public string Name { get; set; }
     public string SessionIdentifier { get; set; }
     public string WorldIdentifier { get; set; }

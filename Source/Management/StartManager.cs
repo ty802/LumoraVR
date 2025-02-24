1a9eca22e0c2d878ff2e91b2f8182ffbc5c6d8c6
ty802 <hfhbrbjfnrjivjekcjejdkntnwiw@thatnameisnotavailable.com>
2025-02-24 13:36:04 -0600

    Do start better

--- Source/Management/StartManager.cs
+++ Source/Management/StartManager.cs
@@ -8,20 +8,18 @@ namespace Aquamarine.Source.Management
     public partial class StartManager : Node
     {
         [Export] public Label Text;
-
-        public override void _Process(double delta)
+        public override void _Ready()
         {
+            base._Ready();
             try
             {
-                base._Process(delta);
-                
                 var isServer = ServerManager.CurrentServerType is not ServerManager.ServerType.NotAServer;
 
                 Text.Text = isServer.ToString();
                 Logger.Log($"Application started in {(isServer ? "server" : "client")} mode.");
 
                 GetTree().ChangeSceneToFile(isServer ? "res://Scenes/Server.tscn" : "res://Scenes/Client.tscn");
-                //Logger.Log($"Scene changed to {(isServer ? "Server.tscn" : "Client.tscn")}.");
+                Logger.Log($"Scene changed to {(isServer ? "Server.tscn" : "Client.tscn")}.");
             }
             catch (Exception ex)
             {

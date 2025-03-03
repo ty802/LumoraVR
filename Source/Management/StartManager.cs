2139271846b5865a8834a5fdc13e26bd53a50e99
xLinka <Oliver.dunnington@outlook.com>
2025-03-03 18:05:56 +0000

    Update to 4.4 and fix Scene Load on Start Manager

--- Source/Management/StartManager.cs
+++ Source/Management/StartManager.cs
@@ -18,13 +18,19 @@ namespace Aquamarine.Source.Management
                 Text.Text = isServer.ToString();
                 Logger.Log($"Application started in {(isServer ? "server" : "client")} mode.");
 
-                GetTree().ChangeSceneToFile(isServer ? "res://Scenes/Server.tscn" : "res://Scenes/Client.tscn");
-                Logger.Log($"Scene changed to {(isServer ? "Server.tscn" : "Client.tscn")}.");
+                // Defer scene change to avoid modifying the scene tree immediately
+                CallDeferred(nameof(ChangeScene), isServer);
             }
             catch (Exception ex)
             {
                 Logger.Error($"Error in StartManager: {ex.Message}");
             }
+        }
+
+        private void ChangeScene(bool isServer)
+        {
+            GetTree().ChangeSceneToFile(isServer ? "res://Scenes/Server.tscn" : "res://Scenes/Client.tscn");
+            Logger.Log($"Scene changed to {(isServer ? "Server.tscn" : "Client.tscn")}.");
         }
     }
 }

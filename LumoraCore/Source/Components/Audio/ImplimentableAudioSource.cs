e19d97d9008bbff43f30cdceeccbd490cec5d383
ty802 <31832680+ty802@users.noreply.github.com>
2025-12-11 15:47:50 -0600

    iterate iterate iterate

--- LumoraCore/Source/Components/Audio/ImplimentableAudioSource.cs
+++ LumoraCore/Source/Components/Audio/ImplimentableAudioSource.cs
@@ -2,10 +2,12 @@ namespace Lumora.Core.Components.Audio;
 
 public class ImplimentableAudioSource : ImplementableComponent
 {
-    public Sync<float>? _volumeDb;
+    public Sync<float>? VolumeDb { get; private set; }
+    public Sync<AudioCategory>? Category { get; private set; }
     public override void OnAwake()
-        {
-            base.OnAwake();
-            _volumeDb = new(this,0);
-        }
+    {
+        base.OnAwake();
+        VolumeDb = new(this, 0);
+        Category = new(this,AudioCategory.Effects);
+    }
 }

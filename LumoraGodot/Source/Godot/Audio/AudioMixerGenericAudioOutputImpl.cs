4cadafb8ea2d109ec780b68398d4f817a1d76845
ty802 <31832680+ty802@users.noreply.github.com>
2026-01-21 17:35:44 -0600

    test123

--- LumoraGodot/Source/Godot/Audio/AudioMixerGenericAudioOutputImpl.cs
+++ LumoraGodot/Source/Godot/Audio/AudioMixerGenericAudioOutputImpl.cs
@@ -6,44 +6,13 @@ namespace Aquamarine.Source.Godot;
 
 public partial class AudioMixer : IAudioMixer
 {
-
-    public string[] GetAvailableAudioEffects()
-    {
-        throw new NotImplementedException();
-    }
-    public static AudioMixer GetMixer() => _instance;
-    private static readonly AudioMixer _instance = new AudioMixer();
-    private readonly object _writeLock = new object();
-    private readonly Dictionary<string, AudioBus> buses = new();
-    private AudioMixer() { ForceSync(); }
-    public void ForceSync()
-    {
-        foreach (var kv in buses)
-        {
-            kv.Value.Invalidate();
-        }
-        lock (_writeLock)
-        {
-
-            Lumora.Core.Logging.Logger.Log($"[Audio][ForceSync] There are {AudioServer.BusCount} buses in the engine");
-            buses.Clear();
-            for (int i = 0; i < AudioServer.BusCount; i++)
-            {
-                string name = AudioServer.GetBusName(i);
-                Lumora.Core.Logging.Logger.Log($"[Audio][ForceSync] Adding bus {name}");
-                var bus = new AudioBus(i);
-                Lumora.Core.Logging.Logger.Log($"[Audio][ForceSync] the bus exists and its name is {bus.Name}");
-                buses.Add(name, bus);
-            }
-        }
-    }
-    public IAudioBus GetAudioBusByName(string name)
+    IAudioBus IAudioMixer.GetAudioBusByName(string name) 
     {
         if (buses.TryGetValue(name, out var b))
             return b;
         throw new Exception("I told you so");
     }
-    public bool TryGetAudioBusByName(string name, out IAudioBus bus)
+    bool IAudioMixer.TryGetAudioBusByName(string name, out IAudioBus bus) 
     {
 #if DEBUG
         void logfname1(string log, [System.Runtime.CompilerServices.CallerFilePath] string fp = "", [System.Runtime.CompilerServices.CallerLineNumber] int ln = 1)
@@ -77,14 +46,14 @@ public partial class AudioMixer : IAudioMixer
         return true;
     }
 #nullable enable
-    public IAudioBus? GetAudioBusByNameOrNull(string name)
+    IAudioBus? IAudioMixer.GetAudioBusByNameOrNull(string name) 
     {
         if (buses.TryGetValue(name, out var bus))
             return (IAudioBus)bus;
         return null;
     }
 #nullable disable
-    public bool CreateAudioBus(string name, out IAudioBus bus)
+    bool IAudioMixer.CreateAudioBus(string name, out IAudioBus bus)
     {
         lock (_writeLock)
         {
@@ -103,7 +72,7 @@ public partial class AudioMixer : IAudioMixer
             return true;
         }
     }
-    public IAudioBus[] GetAllBuses()
+    IAudioBus[] IAudioMixer.GetAllBuses() 
     {
         IAudioBus[] res = new IAudioBus[buses.Count];
         HashSet<int> indexs = new();
---
+++ LumoraGodot/Source/Godot/Audio/LocalAudioStream.cs
@@ -0,0 +1,6 @@
+using Godot;
+using Lumora.Core.External.Audio.Capture;
+using Lumora.Core.External.Audio.Capture.Godot;
+namespace Aquamarine.Source.Godot;
+public partial class LocalAudioSteam : ILocalAudioStream {
+}

38b9f2d9148f24c51885e1fb4bd27ac3cf7136de
ty802 <31832680+ty802@users.noreply.github.com>
2026-01-21 18:07:47 -0600

    more stuff i messed up wile untangling local commit history

--- LumoraCore/Source/External/Audio/Capture/Godot/IGodotAudioCaptureManager.cs
+++ LumoraCore/Source/External/Audio/Capture/Godot/IGodotAudioCaptureManager.cs
@@ -2,4 +2,7 @@ namespace Lumora.Core.External.Audio.Capture.Godot;
 
 public interface IGodotAudioCaptureManager
 {
+    public string[] GetCaptureDeviceNames() ;
+
+    public ILocalAudioStream? GetStreamForOrNull(string captureName);
 }

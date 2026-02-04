07dffc38e47e2850301b2f3301f24435814440a8
ty802 <31832680+ty802@users.noreply.github.com>
2026-02-04 14:20:50 -0600

    better now

--- LumoraCore/Source/Core/Result.cs
+++ LumoraCore/Source/Core/Result.cs
@@ -1,10 +1,20 @@
+using System;
 namespace Lumora.Core
 {
-    public record Result<Tin, TError> where TError : System.Enum
+    public record ResultEnum<Tin, TError> where TError : System.Enum
     {
-        public Result(Tin value) { Value = Value; }
-        public Result(TError error) { TypedError = error; }
-        public TError? TypedError { get; private set; }
+        public ResultEnum(Tin value){
+            this.Value = value;
+            this.IsSuccess = true;
+        }
+        public ResultEnum(TError error){
+            this.TypedError = error;
+            this.IsSuccess = false;
+        }
+        public bool IsSuccess { get; }
+        public TError? TypedError { get; }
         public Tin? Value { get; }
+        public static implicit operator ResultEnum<Tin, TError>(Tin value) => new(value);
+        public static implicit operator ResultEnum<Tin, TError>(TError error) => new(error);
     }
 }

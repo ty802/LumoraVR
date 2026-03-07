9e3562a7c47ff45b0fd39cc16198b506635b49bf
ty802 <31832680+ty802@users.noreply.github.com>
2026-03-07 01:56:47 -0600

    add license statment to all code files

--- LumoraCore/Source/Networking/Messages/ControlMessage.cs
+++ LumoraCore/Source/Networking/Messages/ControlMessage.cs
@@ -1,3 +1,6 @@
+// Copyright (c) 2026 LUMORAVR LTD. All rights reserved.
+// Licensed under the LumoraVR Source Available License. See LICENSE in the project root.
+
 using System.IO;
 
 namespace Lumora.Core.Networking.Messages;

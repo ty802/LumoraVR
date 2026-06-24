2e2f9261278b9dceb14038d8c327eb9a8673b8e8
xLinka <Oliver.dunnington@outlook.com>
2026-06-24 22:57:22 +0100

    Remove Legacy assets

--- LumoraCore/Source/Assets/LumAssets.cs
+++ LumoraCore/Source/Assets/LumAssets.cs
@@ -17,35 +17,7 @@ public static class LumAssets
         public static string Bootstrap => "res://Scenes/UI/Core/Bootstrap.tscn";
         public static string LoadingScreen => "res://Scenes/LoadingScreen.tscn";
 
-        // Inspectors
-        public static string UserInspector => "res://Scenes/UI/Inspectors/UserInspector.tscn";
-        public static string MaterialOrbInspector => "res://Scenes/UI/Inspectors/MaterialOrbInspector.tscn";
-        public static string MaterialColorPicker => "res://Scenes/UI/Inspectors/MaterialColorPicker.tscn";
-        public static string SlotInspector => "res://Scenes/UI/Inspectors/SlotInspector.tscn";
-        public static string ComponentInspector => "res://Scenes/UI/Inspectors/ComponentInspector.tscn";
-        public static string SceneInspector => "res://Scenes/UI/Inspectors/SceneInspector.tscn";
-        public static string ComponentAttacher => "res://Scenes/UI/Inspectors/ComponentAttacher.tscn";
-        public static string ListEditor => "res://Scenes/UI/Inspectors/ListEditor.tscn";
-        public static string ColorPickerPanel => MaterialColorPicker;
-
-        // Debug
-        public static string EngineDebug => "res://Scenes/UI/Debug/EngineDebug.tscn";
+        // Debug console (--lumora-debug)
         public static string DebugWindow => "res://Scenes/UI/Debug/DebugWindow.tscn";
-
-        // Dashboard
-        public static string HomeDash => "res://Scenes/UI/Dashboard/HomeDash.tscn";
-        public static string WorldBrowser => "res://Scenes/UI/Dashboard/WorldBrowser.tscn";
-
-        // Dialogs
-        public static string ImportDialog => "res://Scenes/UI/Dialogs/ImportDialog.tscn";
-
-        // Input
-        public static string VRKeyboard => "res://Scenes/UI/Input/VRKeyboard.tscn";
-        public static string VRKeyboardKey => "res://Scenes/UI/Input/VRKeyboardKey.tscn";
-
-        // Components
-        public static string WorldCard => "res://Scenes/UI/Components/WorldCard.tscn";
-        public static string CategoryButton => "res://Scenes/UI/Components/CategoryButton.tscn";
-        public static string ImportOptionButton => "res://Scenes/UI/Components/ImportOptionButton.tscn";
     }
 }
